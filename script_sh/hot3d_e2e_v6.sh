5e5cf66bb4512c8dfc6d0fb3e1d6cbb87227cf60
Zhifan <zhifan.zhu@bristol.ac.uk>
2026-01-14 11:23:26 +0000

    Add comment on segmentation strategy in script
    Added a comment about the segmentation strategy used in arXiv results.

--- script_sh/hot3d_e2e_v6.sh
+++ script_sh/hot3d_e2e_v6.sh
@@ -5,3 +5,4 @@ HYDRA_FULL_ERROR=1 python potim/run_e2e_v6_segwise_ondemand.py \
     hydra.run.dir=outputs/(date "+%Y-%m-%d")-hot3d/e2e_v6 \
     dataset.static_init_method='multi_upright' \
     optim_mv.num_iters=100
+    # segi_strategy='circle', in arXiv results are run with 'circle'
